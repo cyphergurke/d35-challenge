7444b80c38d605fc1ee44983ab0a26b2e98e1945
cyphergurke <cyberzuchini@protonmail.com>
2026-02-24 12:33:12 +0100

    chore(ui): add sonner toaster integration

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { Sonner } from '@/components/ui/sonner'
 import AutoShopFilterWidget from './widgets/filter/AutoShopFilterWidget.vue'
 </script>
 
 <template>
   <AutoShopFilterWidget />
+  <Sonner />
 </template>
